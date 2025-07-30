c71cc914a2833dd31e65db831426d20af63a2724
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2025-07-30 16:52:38 +0300

    feat(frontend): hamburger menu

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Renderer2, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,4 +8,32 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private document = inject(DOCUMENT);
+  private renderer = inject(Renderer2);
+
+  isMenuOpen = signal(false);
+
+  links = [
+    { title: 'Workspaces', href: '/workspaces' },
+    { title: 'My bookings', href: '/bookings' },
+  ];
+
+  onMenuIconClick(): void {
+    this.isMenuOpen.update((prevIsMenuOpen) => !prevIsMenuOpen);
+    this.toggleBodyClass();
+  }
+
+  onLinkClick(): void {
+    this.isMenuOpen.set(false);
+    this.toggleBodyClass();
+  }
+
+  private toggleBodyClass(): void {
+    if (this.isMenuOpen()) {
+      this.renderer.addClass(this.document.body, 'overflow-hidden');
+    } else {
+      this.renderer.removeClass(this.document.body, 'overflow-hidden');
+    }
+  }
+}
